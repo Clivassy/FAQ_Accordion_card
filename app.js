6be624d2ce72d2c140cb756ef4b8352ab05bd403
Julia Batoro <juliabatoro@MacBook-Pro-de-Julia.local>
2022-01-23 22:59:17 +0100

    js ok

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-// Select Elements from the DOM
-const questions = document.querySelectorAll('.question');
+this.addEventListener("DOMContentLoaded", () =>{
+    const questions = document.querySelectorAll(".question")
+    questions.forEach((question) => question.addEventListener("click", () =>{
+  
+        if(question.parentNode.classList.contains("active")){
+            question.parentNode.classList.toggle("active")
+        }
+    else{
+    questions.forEach(question => question.parentNode.classList.remove("active"))
+    question.parentNode.classList.add("active")
+        }
 
-// Loop through all the elements and add an event listener to them
-questions.forEach((question) => 
-    question.addEventListener('click',() => {
-     if(question.parentNode.classList.contains('active')) {
-        question.parentNode.classList.toggle('active');
-    } else {
-        question.forEach((question) => question.parentNode.
-        classList.remove('active'));
-            question.parentNode.classList.add('active');
-    }
-    })
-);
+    }))
+}) 
